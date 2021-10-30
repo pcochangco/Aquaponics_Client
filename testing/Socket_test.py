b216e73b0fb717678704666d237e5e18226565bc
Philip Justin Cochangco <88139560+pcochangco@users.noreply.github.com>
2021-10-30 09:01:05 +0800

    Update Socket_test.py

--- testing/Socket_test.py
+++ testing/Socket_test.py
@@ -11,7 +11,7 @@ import time
 
 host = 'replace with ip of server (raspi with camera)'
 port = 1403
-execution_wait_time = 3600 # set to 1Hr interval
+execution_wait_time = 1 #3600 set to 1Hr interval
 
 
 
@@ -20,21 +20,21 @@ def setupClient_to_ServerConnection():
     s.connect((host,port))
     return s
     
-#def handler(signum, frame):
-#    raise Exception()
-#signal.signal(signal.SIGALRM, handler) 
+def handler(signum, frame):
+    raise Exception()
+signal.signal(signal.SIGALRM, handler) 
 
 while True:
     try: 
         s = setupClient_to_ServerConnection()
-        #user = input("")
+        user = input("")
         client_data = "This is the CLient: What is the lettuce area ?"
         s.send(str.encode(client_data)
        
         # this line is where the client hear back from server
-        #signal.alarm(120) #set timer to 2mins if the server took long to send data - redo the connection
+        signal.alarm(120) #set timer to 2mins if the server took long to send data - redo the connection
         lettuce_area = s.recv(1024)
-        #signal.alarm(0)
+        signal.alarm(0)
        
         s.close()
         print("Data from server (lettuce area): ",lettuce_area.decode('utf-8'))
